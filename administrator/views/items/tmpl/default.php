067cd1f4ddd640a99a38f500d0714c7084563131
A.Waasdorp <A.Waasdorp@192.168.1.14>
2018-02-28 19:19:51 +0100

    0.0.1 items default

--- administrator/views/items/tmpl/default.php
+++ administrator/views/items/tmpl/default.php
@@ -1,49 +1,52 @@
 <?php 
 /**
  * @version $Id: default.php 48 2017-08-04 11:41:27Z szymon $
- * @package DJ-ImageSlider
- * @subpackage DJ-ImageSlider Component
- * @copyright Copyright (C) 2017 DJ-Extensions.com, All rights reserved.
+ * @package WsaCarousel
+ * @subpackage WsaCarousel Component
+ * @copyright Copyright (C) 2017 waasdorpsoekhan.nl, All rights reserved.
  * @license http://www.gnu.org/licenses GNU/GPL
- * @author url: http://dj-extensions.com
- * @author email contact@dj-extensions.com
- * @developer Szymon Woronowski - szymon.woronowski@design-joomla.eu
+ * @author url: https://www.waasdorpsoekhan.nl
+ * @author email contact@waasdorpsoekhan.nl
+ * @developer A.H.C. Waasdorp
  *
  *
- * DJ-ImageSlider is free software: you can redistribute it and/or modify
+ * WsaCarousel is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DJ-ImageSlider is distributed in the hope that it will be useful,
+ * WsaCarousel is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DJ-ImageSlider. If not, see <http://www.gnu.org/licenses/>.
+ * along with WsaCarousel. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
-defined('_JEXEC') or die('Restricted access'); ?>
+defined('_JEXEC') or die('Restricted access'); 
 
-<?php 
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
+
 $isJoomla3 = version_compare(JVERSION, '3.0', '>=');
-JHTML::_('behavior.tooltip');
-JHTML::_('behavior.modal');
-if($isJoomla3) JHtml::_('formbehavior.chosen', 'select');
+HTMLHelper::_('behavior.tooltip');
+HTMLHelper::_('behavior.modal');
+if($isJoomla3) HTMLHelper::_('formbehavior.chosen', 'select');
 
-$user		= JFactory::getUser();
+$user		= Factory::getUser();
 $userId		= $user->get('id');
 $listOrder	= $this->state->get('list.ordering');
 $listDirn	= $this->state->get('list.direction');
-$canOrder	= $user->authorise('core.edit.state', 'com_djimageslider.category');
+$canOrder	= $user->authorise('core.edit.state', 'COM_WSACAROUSEL.category');
 $saveOrder	= $listOrder == 'a.ordering';
 
 if ($isJoomla3 && $saveOrder)
 {
-	$saveOrderingUrl = 'index.php?option=com_djimageslider&task=items.saveOrderAjax&tmpl=component';
-	JHtml::_('sortablelist.sortable', 'slidesList', 'adminForm', strtolower($listDirn), $saveOrderingUrl);
+	$saveOrderingUrl = 'index.php?option=COM_WSACAROUSEL&task=items.saveOrderAjax&tmpl=component';
+	HTMLHelper::_('sortablelist.sortable', 'slidesList', 'adminForm', strtolower($listDirn), $saveOrderingUrl);
 }
 ?>
 
@@ -56,11 +59,11 @@ if ($isJoomla3 && $saveOrder)
 <div id="j-main-container">
 <?php endif;?>
 	
-<form action="<?php echo JRoute::_('index.php?option=com_djimageslider&view=items'); ?>" method="post" name="adminForm" id="adminForm">
+<form action="<?php echo Route::_('index.php?option=COM_WSACAROUSEL&view=items'); ?>" method="post" name="adminForm" id="adminForm">
 	<fieldset id="filter-bar" class="btn-toolbar">
 		<div class="filter-search fltlft btn-group pull-left">
 			<label class="filter-search-lbl element-invisible" for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
-			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" placeholder="<?php echo JText::_('COM_DJIMAGESLIDER_SEARCH_IN_TITLE'); ?>" />
+			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" placeholder="<?php echo JText::_('COM_WSACAROUSEL_SEARCH_IN_TITLE'); ?>" />
 		</div>
 		<div class="filter-search fltlft btn-group pull-left">
 			<button type="submit" class="btn"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
@@ -72,13 +75,13 @@ if ($isJoomla3 && $saveOrder)
 		<div class="filter-select fltrt btn-group pull-right">
 			<select name="filter_published" class="inputbox input-medium" onchange="this.form.submit()">
 				<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
-				<?php echo JHtml::_('select.options', array(JHtml::_('select.option', '1', 'JPUBLISHED'),JHtml::_('select.option', '0', 'JUNPUBLISHED')), 'value', 'text', $this->state->get('filter.published'), true);?>
+				<?php echo HTMLHelper::_('select.options', array(HTMLHelper::_('select.option', '1', 'JPUBLISHED'),HTMLHelper::_('select.option', '0', 'JUNPUBLISHED')), 'value', 'text', $this->state->get('filter.published'), true);?>
 			</select>
 		</div>
 		<div class="filter-select fltrt btn-group pull-right">
 			<select name="filter_category" class="inputbox" onchange="this.form.submit()">
 				<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
-				<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_djimageslider'), 'value', 'text', $this->state->get('filter.category'));?>
+				<?php echo HTMLHelper::_('select.options', HTMLHelper::_('category.options', 'COM_WSACAROUSEL'), 'value', 'text', $this->state->get('filter.category'));?>
 			</select>
 		</div>
 	</fieldset>
@@ -89,34 +92,34 @@ if ($isJoomla3 && $saveOrder)
 			<tr>
 			<?php if($isJoomla3) { ?>
 				<th width="1%" class="nowrap center hidden-phone">
-					<?php echo JHtml::_('grid.sort', '<i class="icon-menu-2"></i>', 'a.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING'); ?>
+					<?php echo HTMLHelper::_('grid.sort', '<i class="icon-menu-2"></i>', 'a.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING'); ?>
 				</th>
 			<?php } ?>
 				<th width="1%">
 					<input type="checkbox" name="checkall-toggle" value="" onclick="checkAll(this)" />
 				</th>
 				<th width="8%">
-					<?php echo JText::_('COM_DJIMAGESLIDER_IMAGE'); ?>
+					<?php echo JText::_('COM_WSACAROUSEL_IMAGE'); ?>
 				</th>
 				<th>
-					<?php echo JHtml::_('grid.sort',  'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+					<?php echo HTMLHelper::_('grid.sort',  'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
 				</th>				
 				<th width="5%">
-					<?php echo JHtml::_('grid.sort', 'JPUBLISHED', 'a.published', $listDirn, $listOrder); ?>
+					<?php echo HTMLHelper::_('grid.sort', 'JPUBLISHED', 'a.published', $listDirn, $listOrder); ?>
 				</th>
 			<?php if(!$isJoomla3) { ?>
 				<th width="10%">
-					<?php echo JHtml::_('grid.sort',  'JGRID_HEADING_ORDERING', 'a.ordering', $listDirn, $listOrder); ?>
+					<?php echo HTMLHelper::_('grid.sort',  'JGRID_HEADING_ORDERING', 'a.ordering', $listDirn, $listOrder); ?>
 					<?php if ($canOrder && $saveOrder) :?>
-						<?php echo JHtml::_('grid.order',  $this->items, 'filesave.png', 'items.saveorder'); ?>
+						<?php echo HTMLHelper::_('grid.order',  $this->items, 'filesave.png', 'items.saveorder'); ?>
 					<?php endif; ?>
 				</th>
 			<?php } ?>
 				<th width="10%">
-					<?php echo JHtml::_('grid.sort', 'JCATEGORY', 'category_title', $listDirn, $listOrder); ?>
+					<?php echo HTMLHelper::_('grid.sort', 'JCATEGORY', 'category_title', $listDirn, $listOrder); ?>
 				</th>
 				<th width="1%">
-					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+					<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
 				</th>
 			</tr>
 		</thead>
@@ -132,11 +135,11 @@ if ($isJoomla3 && $saveOrder)
 		$n = count($this->items);
 		foreach ($this->items as $i => $item) :
 			$ordering	= ($listOrder == 'a.ordering');
-			$canCreate	= $user->authorise('core.create',		'com_djimageslider.category.'.$item->catid);
-			$canEdit	= $user->authorise('core.edit',			'com_djimageslider.category.'.$item->catid);
+			$canCreate	= $user->authorise('core.create',		'COM_WSACAROUSEL.category.'.$item->catid);
+			$canEdit	= $user->authorise('core.edit',			'COM_WSACAROUSEL.category.'.$item->catid);
 			$canCheckin	= $user->authorise('core.manage',		'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
-			$canEditOwn	= true; //$user->authorise('core.edit.own',		'com_djimageslider.category.'.$item->catid) && $item->created_by == $userId;
-			$canChange	= $user->authorise('core.edit.state',	'com_djimageslider.category.'.$item->catid) && $canCheckin;
+			$canEditOwn	= true; //$user->authorise('core.edit.own',		'COM_WSACAROUSEL.category.'.$item->catid) && $item->created_by == $userId;
+			$canChange	= $user->authorise('core.edit.state',	'COM_WSACAROUSEL.category.'.$item->catid) && $canCheckin;
 
 			?>
 			<tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->catid?>">
@@ -150,7 +153,7 @@ if ($isJoomla3 && $saveOrder)
 							}
 							elseif (!$saveOrder)
 							{
-								$iconClass = ' inactive tip-top hasTooltip" title="' . JHtml::tooltipText('JORDERINGDISABLED');
+							    $iconClass = ' inactive tip-top hasTooltip" title="' . HTMLHelper::tooltipText('JORDERINGDISABLED');
 							}
 							?>
 							<span class="sortable-handler<?php echo $iconClass ?>">
@@ -162,7 +165,7 @@ if ($isJoomla3 && $saveOrder)
 				</td>
 			<?php } ?>
 				<td class="center">
-					<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+					<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
 				</td>
 				<td align="center">
 					<?php if ($item->image) : ?>
@@ -171,10 +174,10 @@ if ($isJoomla3 && $saveOrder)
 				</td>
 				<td>
 					<?php if ($item->checked_out) : ?>
-						<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'items.', $canCheckin); ?>
+						<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'items.', $canCheckin); ?>
 					<?php endif; ?>
 					<?php if ($canEdit || $canEditOwn) : ?>
-						<a href="<?php echo JRoute::_('index.php?option=com_djimageslider&task=item.edit&id='.(int) $item->id); ?>">
+						<a href="<?php echo Route::_('index.php?option=COM_WSACAROUSEL&task=item.edit&id='.(int) $item->id); ?>">
 							<?php echo $this->escape($item->title); ?></a>
 					<?php else : ?>
 						<?php echo $this->escape($item->title); ?>
@@ -190,7 +193,7 @@ if ($isJoomla3 && $saveOrder)
 					</div>
 				</td>
 				<td class="center">
-					<?php echo JHtml::_('jgrid.published', $item->published, $i, 'items.', true, 'cb'	); ?>
+					<?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'items.', true, 'cb'	); ?>
 				</td>
 			<?php if(!$isJoomla3) { ?>
 				<td class="order" nowrap="nowrap">
@@ -226,7 +229,7 @@ if ($isJoomla3 && $saveOrder)
 		<input type="hidden" name="boxchecked" value="0" />
 		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
 		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-		<?php echo JHtml::_('form.token'); ?>
+		<?php echo HTMLHelper::_('form.token'); ?>
 	</div>
 </form>
 </div>
