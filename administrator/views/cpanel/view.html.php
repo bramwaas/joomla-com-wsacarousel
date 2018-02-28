96b972879202c77f69afb66901699e6618dcd3ab
A.Waasdorp <A.Waasdorp@192.168.1.14>
2018-02-28 20:04:19 +0100

    0.0.1 view cpanel

--- administrator/views/cpanel/view.html.php
+++ administrator/views/cpanel/view.html.php
@@ -1,43 +1,48 @@
 <?php 
 /**
  * @version $Id: view.html.php 48 2017-08-04 11:41:27Z szymon $
- * @package DJ-ImageSlider
- * @subpackage DJ-ImageSlider Component
- * @copyright Copyright (C) 2017 DJ-Extensions.com, All rights reserved.
+ * @package WsaCarousel
+ * @subpackage WsaCarousel Component
+ * @copyright Copyright (C) 2017 waasdorpsoekhan.nl, All rights reserved.
  * @license http://www.gnu.org/licenses GNU/GPL
- * @author url: http://dj-extensions.com
- * @author email contact@dj-extensions.com
- * @developer Szymon Woronowski - szymon.woronowski@design-joomla.eu
+ * @author url: http://waasdorpsoekhan.nl
+ * @author email contact@waasdorpsoekhan.nl
+ * @developer A.H.C. Waasdorp
  *
  *
- * DJ-ImageSlider is free software: you can redistribute it and/or modify
+ * WsaCarousel is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DJ-ImageSlider is distributed in the hope that it will be useful,
+ * WsaCarousel is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DJ-ImageSlider. If not, see <http://www.gnu.org/licenses/>.
+ * along with WsaCarousel. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
 defined('_JEXEC') or die( 'Restricted access' );
+//use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\View\HtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+//use Joomla\CMS\Uri;
+//use Joomla\CMS\Language\Text;
 
 jimport( 'joomla.application.component.view');
 jimport( 'joomla.application.categories');
 jimport('joomla.html.pane');
 
-class DJImageSliderViewCpanel extends JViewLegacy
+class WsaCarouselViewCpanel extends HtmlView
 {
 	function display($tpl = null)
 	{
-		JToolBarHelper::title( JText::_('COM_DJIMAGESLIDER'));
+		ToolBarHelper::title( JText::_('COM_WSACAROUSEL'));
 		
-		JToolBarHelper::preferences('com_djimageslider', 550, 875);
+		ToolBarHelper::preferences('COM_WSACAROUSEL', 550, 875);
 		
 		if (class_exists('JHtmlSidebar')){
 			$this->sidebar = JHtmlSidebar::render();
