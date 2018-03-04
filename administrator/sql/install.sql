958a26abca446d805fd16447fe436f13c7189671
A.Waasdorp <A.Waasdorp@192.168.1.14>
2018-03-04 14:13:17 +0100

    0.0.1 SQL

--- administrator/sql/install.sql
+++ administrator/sql/install.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `#__djimageslider` (
+CREATE TABLE IF NOT EXISTS `#__wsacarousel` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `catid` int(10) unsigned NOT NULL default '0',
   `title` varchar(255) NOT NULL,
