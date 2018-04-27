7a9b0afa4b2cc72b288b7673d5d0a3281c9723a6
A.Waasdorp <A.Waasdorp@192.168.1.13>
2018-04-27 20:24:49 +0200

    0.0.4 CpanelModel

---
+++ administrator/Model/CpanelModel.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @version 0.0.4 $Id: CpanelModel.php 
+ * @package     Joomla.Administrator
+ * @subpackage  com_wsacarousel
+ * @copyright Copyright (C) 2017 waasdorpsoekhan.nl, All rights reserved.
+ * @license http://www.gnu.org/licenses GNU/GPL
+ * @author url: https://waasdorpsoekhan.nl
+ * @author email contact@waasdorpsoekhan.nl
+ * @developer A.H.C. Waasdorp
+ *
+ *
+ * WsaCarousel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WsaCarousel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WsaCarousel. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace Joomla\Component\Wsacarousel\Administrator\Model;
+
+defined('_JEXEC') or die;
+//use Joomla\CMS\Factory;
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+
+//jimport( 'joomla.application.component.model');
+
+
+class CpanelModel extends BaseDatabaseModel {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+}
+?>
