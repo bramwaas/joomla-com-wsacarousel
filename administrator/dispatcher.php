8f85cd4a9c7c803b02d9d746f7fe108c9f78f9e4
ahcwa <ahcwa@bram-len.fritz.box>
2022-01-04 19:47:53 +0100

    1.0.1

--- administrator/dispatcher.php
+++ administrator/dispatcher.php
@@ -5,6 +5,7 @@
  *
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * Waarschijnlijk overbodig
  */
 
 defined('_JEXEC') or die;
@@ -26,5 +27,5 @@ class WsacarouselDispatcher extends Dispatcher
 	 * @since  4.0.0
 	 */
     
-	protected $namespace = 'Joomla\\Component\\Wsacarousel';
+	protected $namespace = 'WaasdorpSoekhan\\Component\\Wsacarousel';
 }
