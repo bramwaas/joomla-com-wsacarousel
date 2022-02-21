85b8f686d1fc78ebda82bcb68ca597f23ce447b9
ahcwa <ahcwa@bram-len.fritz.box>
2022-02-21 17:45:14 +0100

    1.0.6

--- administrator/sql/updates/1.0.6.sql
+++ administrator/sql/updates/1.0.6.sql
@@ -1,11 +1,10 @@
 --
 -- This file will contain Table structure for `"__wsacarousel`
--- v 1.0.4 DROP some  NULL constraint from `publish_down`
--- 2022 01 07
+-- v 1.0.6 again try to DROP some  NULL constraint from `publish_down` and others
+-- 2022 02 21
 --
-ALTER TABLE `#__wsacarousel` MODIFY `publish_up` datetime;
-ALTER TABLE `#__wsacarousel` MODIFY `publish_down` datetime;
-ALTER TABLE `#__wsacarousel` MODIFY `checked_out_time` datetime;
-ALTER TABLE `#__wsacarousel` MODIFY `checked_out` int(10) unsigned;
-ALTER TABLE `#__wsacarousel` MODIFY `modified` datetime;
-
+ALTER TABLE `#__wsacarousel` MODIFY `publish_up` datetime NULL;
+ALTER TABLE `#__wsacarousel` MODIFY `publish_down` datetime NULL;
+ALTER TABLE `#__wsacarousel` MODIFY `checked_out_time` datetime NULL;
+ALTER TABLE `#__wsacarousel` MODIFY `checked_out` int(10) unsigned NULL;
+ALTER TABLE `#__wsacarousel` MODIFY `modified` datetime NULL;
